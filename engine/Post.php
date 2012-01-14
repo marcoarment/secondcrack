7eac0dae669774151ad875dc33db4bba43feccc5
Marco Arment <me@marco.org>
2012-01-14 12:23:20 -0500

    Fixed multiple-tag parsing bug

--- engine/Post.php
+++ engine/Post.php
@@ -263,7 +263,11 @@ class Post
 
     public static function parse_tag_str($tag_str)
     {
-        $tags = array_unique(array_filter(array_map('trim', explode(',', str_replace(' ', '_', strtolower($tag_str)))), 'strlen'));
+		// Tags are comma-separated, and any spaces between multiple words in a tag will be converted to underscores for URLs
+		if (! strlen($tag_str)) return array();
+		$tags = array_map('trim', explode(',', strtolower($tag_str)));
+		$tags = str_replace(' ', '_', $tags);
+        $tags = array_unique(array_filter($tags, 'strlen'));
         sort($tags);
         return $tags;
     }
