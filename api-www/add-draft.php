2e2f29d7f233b692d6cb3e6bb3c71262525d0c31
Jay Hickey <johnrhickey@gmail.com>
2012-06-17 17:30:42 -0400

    Added a bookmarklet to draft link with embeded Youtube or Vimeo videos

--- api-www/add-draft.php
+++ api-www/add-draft.php
@@ -24,7 +24,8 @@ if (! isset($_GET['u'])) {
     ?>
     <p>
         <a href="javascript:<?= rawurlencode(str_replace('EXTRA', 'is-link=1', $bookmarklet_code)) ?>">Draft Link</a> &bull;
-        <a href="javascript:<?= rawurlencode(str_replace('EXTRA', '', $bookmarklet_code)) ?>">Draft Article</a>
+        <a href="javascript:<?= rawurlencode(str_replace('EXTRA', '', $bookmarklet_code)) ?>">Draft Article</a>  &bull;
+        <a href="javascript:<?= rawurlencode(str_replace('EXTRA', 'is-video=1', $bookmarklet_code)) ?>">Draft Video Link</a>
     </p>
     
     <p>
@@ -35,11 +36,18 @@ if (! isset($_GET['u'])) {
         Draft article code:<br/>
         <textarea>javascript:<?= h(rawurlencode(str_replace('EXTRA', '', $bookmarklet_code))) ?></textarea>
     </p>
+    <p>
+        Draft video link code:<br/>
+        <textarea>javascript:<?= h(rawurlencode(str_replace('EXTRA', 'is-video=1', $bookmarklet_code))) ?></textarea>
+    </p>
     <?
     exit;
 }
 
 $url = substring_before(normalize_space($_GET['u']), ' ');
+$is_video = isset($_GET['is-video']) && intval($_GET['is-video']);
+$html = get_html($url);
+$video = video_embed($html);
 $title = normalize_space($_GET['t']);
 $selection = trim($_GET['s']);
 $is_link = isset($_GET['is-link']) && intval($_GET['is-link']);
@@ -48,15 +56,53 @@ if (! $slug) $slug = 'draft';
 
 if ($selection) {
     $body = "> " . str_replace("\n", "\n> ", trim($selection)) . "\n\n";
-    if (! $is_link) $body = "[$title]($url):\n\n" . $body;
+    if (! ($is_link or $is_video)) $body = "[$title]($url):\n\n" . $body;
 } else {
     $body = '';
+}
+
+if ($is_video) $body = $video . $body;
+
+// Get the HTML with cURL
+function get_html($url) {
+  $ch = curl_init();
+  $timeout = 5;
+  curl_setopt($ch,CURLOPT_URL,$url);
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+  curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+  $data = curl_exec($ch);
+  curl_close($ch);
+  return $data;
+}
+
+function video_embed($html) {
+	// Regex for parsing HTML
+    $vimeo = array('#player.vimeo.com/video/([0-9]*)#i');
+    $youtube = array('#/embed/([0-9a-z_-]+)#i', '#youtu.be/([0-9a-z_-]+)#i', '#/v/([0-9a-z_-]+)#i', '#v=([0-9a-z_-]+)#i');
+
+    // Perform preg_match on Vimeo regex array
+	for ($i = 0; $i < sizeof($vimeo); $i++) {
+		preg_match($vimeo[$i], $html, $id);
+		if ($id[1] != NULL) {
+			return '<iframe src="http://player.vimeo.com/video/' . $id[1] . '?title=0&amp;byline=0&amp;portrait=0&amp;color=ff0000&amp;fullscreen=1&amp;autoplay=0" frameborder="0"></iframe>';	    	
+	    }		
+	}
+	
+	// Perform preg_match on Youtube regex array
+	for ($i = 0; $i < sizeof($youtube); $i++) {
+    	    preg_match($youtube[$i], $html, $id);
+    	    // Return embed URL with video ID if a match is found
+    	    if ($id[1] != NULL) {
+	        return '<iframe src="http://www.youtube.com/embed/' . $id[1] . '?autohide=1&amp;fs=1&amp;autoplay=0&amp;iv_load_policy=3&amp;rel=0&amp;modestbranding=1&amp;showinfo=0&amp;hd=1" frameborder="0" allowfullscreen></iframe>';	
+    	    }
+	}
+	return NULL;	
 }
 
 $draft_contents = 
     $title . "\n" . 
     str_repeat('=', max(10, min(40, strlen($title)))) . "\n" .
-    ($is_link ? "Link: " . $url . "\n" : '') .
+    (($is_link or $is_video) ? "Link: " . $url . "\n" : '') .
     "publish-not-yet\n" .
     "\n" .
     $body
